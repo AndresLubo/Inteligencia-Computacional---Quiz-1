0efede6cf841a8643b4b1b0d23592b508f4ac7ff
AndresLubo <alubo_08@outlook.com>
2022-03-02 19:40:09 -0500

    Agregado el tercer punto

---
+++ Punto3.py
@@ -0,0 +1,5 @@
+sueldoBase = float(input("Ingrese su sueldo base: "))
+
+pagoTotal = sueldoBase + (sueldoBase * 0.15)
+print(f"Su sueldo base es: {sueldoBase}$\nSu comisión es: {sueldoBase * 0.15}$")
+print(f"Su pago total es: {pagoTotal}$")
